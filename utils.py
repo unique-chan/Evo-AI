14fe4ebba3fdddedae8d643fba2e282f898de615
unique-chan <yechankim@gm.gist.ac.kr>
2021-10-27 18:34:27 +0900

    plot +

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import random
 
-import copy
 import numpy as np
 import pandas as pd
 import random as rand
+import matplotlib.pyplot as plt
 
 
 def read_distance_matrix_from_txt(path, header=None, skiprows=4, sep=','):
@@ -15,14 +15,13 @@ def read_distance_matrix_from_txt(path, header=None, skiprows=4, sep=','):
     return distance_matrix
 
 
-def init_population(pop_size=100, gen_size=6000, num_cities=200):
+def init_population(pop_size=100, num_cities=200):
     '''
-    :param pop_size: chromosome (string) size
-    :param gen_size: generation size
+    :param pop_size: # of chromosomes
     :param num_cities: number of total cities
     :return: population (e.g. [ [2,4,1,0,6,9,...], ... ], where integers stand for 'city' indices.)
     '''
-    population = [list(rand.sample(range(num_cities), k=pop_size)) for _ in range(gen_size)]
+    population = [list(rand.sample(range(num_cities), k=num_cities)) for _ in range(pop_size)]
     return population
 
 
@@ -51,8 +50,9 @@ def store(population, fitnesses, filename='test.txt'):
 
 def elitism(population, fitnesses, elitism_ratio):
     num_elites = int(len(population) * elitism_ratio)
-    top_ids = sorted(np.arange(len(fitnesses)), key=lambda i: fitnesses[i])[-num_elites:]
-    bottom_ids = sorted(np.arange(len(fitnesses)), key=lambda i: fitnesses[i])[:num_elites]
+    top_ids = sorted(np.arange(len(fitnesses)), key=lambda i: fitnesses[i], reverse=True)[-num_elites:]
+    bottom_ids = sorted(np.arange(len(fitnesses)), key=lambda i: fitnesses[i], reverse=True)[:num_elites]
+    # reverse=True? -> The objective is to minimize fitness score in TSP.
 
     for i, (top_id, bottom_id) in enumerate(zip(top_ids, bottom_ids)):
         population[bottom_id] = population[top_id]
@@ -64,8 +64,13 @@ def elitism(population, fitnesses, elitism_ratio):
     return elite_chromosomes, population, fitnesses
 
 
-def rank_selection():
-    pass
+def tournament_selection(population, distance_matrix):
+    pop_size = len(population)
+    for i in range(pop_size):
+        pos = random.randint(0, pop_size-1)
+        f_i, f_pos = get_fitness(distance_matrix, population[i]), get_fitness(distance_matrix, population[pos])
+        if is_promising_for_salesman(f_pos, f_i):
+            population[i] = population[pos]
 
 
 def order_one_crossover(population, prob, num_offsprings=1):
@@ -73,31 +78,32 @@ def order_one_crossover(population, prob, num_offsprings=1):
     :param num_offsprings: [1 or 2]
     :return: crossover result
     '''
-    pop_size, gen_size = len(population[0]), len(population)
-    gen_size_half = gen_size // 2
+    string_length, pop_size = len(population[0]), len(population)
+    pop_size_half = pop_size // 2
     random.shuffle(population)   # for bias issue
-    for i in range(gen_size_half):
+    for i in range(pop_size_half):
         if random.random() <= prob:
-            dad, mom = population[i], population[i + gen_size_half]
-            start_idx, end_idx = sorted(np.random.choice(list(range(pop_size)), size=2, replace=False))
+            dad, mom = population[i], population[i + pop_size_half]
+            start_idx, end_idx = sorted(np.random.choice(list(range(string_length)), size=2, replace=False))
             # offspring 1
             partial_dad_1 = dad[start_idx:end_idx+1]
             partial_mom_1 = [gene for gene in mom if gene not in partial_dad_1]
             offspring_1 = [partial_mom_1.pop(0) for _ in range(0, min(len(partial_mom_1), start_idx))] + \
                            partial_dad_1 + partial_mom_1
-            # If pop_size < number of cities, offspring size would not be steady (i.e. pop_size != len(offspring_1)).
+            # If string_length < number of cities, offspring size would not be steady
+            # (i.e. string_length != len(offspring_1)).
             # => In this case,
             # 1) repeat randomly removing genes
-            #    until the offspring size equals to the pop_size.  (if pop_size < len(offspring_1)
-            # 2) discard this offspring!                           (if pop_size > len(offspring_1)
+            #    until the offspring size equals to the string_length.  (if string_length < len(offspring_1)
+            # 2) discard this offspring!                           (if string_length > len(offspring_1)
             # (Yechan Kim's Trick)
-            if pop_size < len(offspring_1):
-                size = len(offspring_1) - pop_size
-                mask = sorted(np.random.choice(list(np.arange(len(offspring_1))), size=size, replace=False))
-                for _, m in enumerate(mask):
-                    del offspring_1[m - _]
-            elif pop_size > len(offspring_1):
-                continue
+            # if string_length < len(offspring_1):
+            #     size = len(offspring_1) - string_length
+            #     mask = sorted(np.random.choice(list(np.arange(len(offspring_1))), size=size, replace=False))
+            #     for _, m in enumerate(mask):
+            #         del offspring_1[m - _]
+            # elif string_length > len(offspring_1):
+            #     continue
             population[i] = offspring_1
             # offspring 2 (If you want to birth another offspring!)
             if num_offsprings >= 2:
@@ -106,23 +112,32 @@ def order_one_crossover(population, prob, num_offsprings=1):
                 offspring_2 = [partial_dad_2.pop(0) for _ in range(0, min(len(partial_dad_2), start_idx))] + \
                                partial_mom_2 + partial_dad_2
                 # (Yechan Kim's Trick) (again)
-                if pop_size < len(offspring_2):
-                    size = len(offspring_2) - pop_size
-                    mask = sorted(np.random.choice(list(np.arange(len(offspring_2))), size=size, replace=False))
-                    for _, m in enumerate(mask):
-                        del offspring_2[m - _]
-                elif pop_size > len(offspring_2):
-                    continue
-                population[i + gen_size_half] = offspring_2
+                # if string_length < len(offspring_2):
+                #     size = len(offspring_2) - string_length
+                #     mask = sorted(np.random.choice(list(np.arange(len(offspring_2))), size=size, replace=False))
+                #     for _, m in enumerate(mask):
+                #         del offspring_2[m - _]
+                # elif string_length > len(offspring_2):
+                #     continue
+                population[i + pop_size_half] = offspring_2
 
 
 def mutation(population, prob):
     '''
     :return: mutation result
     '''
-    pop_size, gen_size = len(population[0]), len(population)
-    for i in range(gen_size):
+    string_length, pop_size = len(population[0]), len(population)
+    for i in range(pop_size):
         if random.random() <= prob:
             # swap
-            cursor_1, cursor_2 = np.random.choice(list(range(pop_size)), size=2, replace=False)
+            cursor_1, cursor_2 = np.random.choice(list(range(string_length)), size=2, replace=False)
             population[i][cursor_1], population[i][cursor_2] = population[i][cursor_2], population[i][cursor_1]
+
+
+def plot(d1, d2, title, name):
+    plt.title(f"Traveling Salesman Problem Fitness Trace ({name})")
+    plt.plot(range(len(d1)), np.log(np.array(d1)), label="Log(Fitness), average")
+    plt.plot(range(len(d2)), np.log(np.array(d2)), label="Log(Fitness), bestgit add")
+    plt.legend()
+    plt.savefig(name)
+    plt.show()
