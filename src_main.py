83bf1b835bda3cb4611da30fa444db426ef7f886
unique-chan <yechankim@gm.gist.ac.kr>
2021-10-31 00:02:13 +0900

    selection +

--- src_main.py
+++ src_main.py
@@ -12,6 +12,8 @@ e.g. python src_main.py data(TSP)/data-1.txt
 pop_size = 100
 gen_size = 6000
 crossover_prob = 0.9
+soft_tournament_prob = 0.7
+# boltzmann_prob = 0.8
 mutation_prob = 0.01
 elitism_ratio = 0.02
 num_offsprings_for_crossover = 2  # 1 or 2
@@ -27,6 +29,9 @@ random.seed(34)
 D = utils.read_distance_matrix_from_txt(file)
 P = utils.init_population(pop_size=pop_size, num_cities=D.shape[0])
 out_file = file.replace('data(TSP)/', '').replace('data-', 'fitness-')  # 'fitness-x.txt' where x is integer.
+
+# for logarithmic scaling on boltzmann_selection / roulette_selection
+# T = 40
 
 best, avg_fitnesses, best_fitnesses = np.infty, [], []
 for i in range(gen_size):
@@ -47,7 +52,46 @@ for i in range(gen_size):
     elite_P, P, fitnesses = utils.elitism(P, fitnesses, elitism_ratio)
 
     # selection
-    utils.tournament_selection(P, D)
+    # [Note] I tried to implement various selection algorithms as in below (A)-(E).
+    # Yechan's opinion:     I strongly recommend you use 'tournament selection.'
+    #                       Especially, please try to use 'soft tournament selection' for your experiments.
+
+    #########################################################################################################
+    # selection option (A): tournament selection - strict version
+    #                                                   -> [opinion] great option for the given dataset!
+    # utils.tournament_selection(P, D)
+    #########################################################################################################
+
+    #########################################################################################################
+    # selection option (B): tournament selection - soft version
+    #                                                   -> [opinion] excellent option for the given dataset!
+    # [Note] All the submission results (fitness-*.txt / fitness-*.png) are based on using (B)!
+    utils.tournament_selection(P, D, soft_tournament_prob=soft_tournament_prob)
+    #########################################################################################################
+
+    #########################################################################################################
+    # selection option (C): boltzmann_selection
+    #                                                   -> [opinion] really terrible for the given dataset!
+    # if i >= 1:
+    #     if utils.is_promising_for_salesman(best_fitnesses[i], best_fitnesses[i-1]):
+    #         T *= 0.99  # it should be tuned!
+    # utils.boltzmann_selection(P, D, boltzmann_prob, T)
+    #########################################################################################################
+
+    #########################################################################################################
+    # selection option (D): roulette selection
+    #                                                   -> [opinion] really terrible for the given dataset!
+    # P = utils.roulette_selection(P, fitnesses)
+    #########################################################################################################
+
+    #########################################################################################################
+    # selection option (E): roulette selection with logarithmic_scaling
+    #                                                   -> [opinion] really terrible for the given dataset!
+    # if i >= 1:
+    #     if utils.is_promising_for_salesman(best_fitnesses[i], best_fitnesses[i-1]):
+    #         T *= 0.99  # it should be tuned!
+    # P = utils.roulette_selection(P, fitnesses, logarithmic_scaling_T=T)
+    #########################################################################################################
 
     # crossover
     utils.order_one_crossover(P, crossover_prob, num_offsprings=num_offsprings_for_crossover)
